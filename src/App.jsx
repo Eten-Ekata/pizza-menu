89d27006bdb5b764ab28e9871ab769d4b422f08c
Eten-Ekata <ekataelishaeten@gmail.com>
2023-07-11 18:54:20 +0100

    css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Header from "./components/Header"
+import Menu from "./components/Menu"
+import Footer from "./components/Footer"
+import "./index.css"
 function App() {
-
-  return (
+    return (
     <div className='container'>
      <Header />
       <Menu />
